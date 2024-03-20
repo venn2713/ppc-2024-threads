9f21c5383a06355f0ad6a57bc41e1a4d76bd91d2
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2024-03-20 10:44:22 +0300

    done funct tests

---
+++ tasks/seq/ivanov_n_int_simpson/func_tests/main.cpp
@@ -0,0 +1,156 @@
+// Copyright 2023 Nesterov Alexander
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "seq/ivanov_n_int_simpson/include/ops_seq.hpp"
+
+TEST(Sequential_ivanov, Linar_0_2) {
+  const int a = 0;
+  const int b = 2;
+  const int c = 0;
+  const int d = 2;
+  const int n = 100;
+  const int res = 8;
+  func fuction = linear_fun;
+
+  // Create data
+  std::vector<int> in = {a, b, c, d, n};
+  std::vector<double> out(1, res);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(fuction));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  TestTaskSequentialSimpson testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_LE(res-out[0], 0.001);
+}
+
+TEST(Sequential_ivanov, x_mul_y_5_10) {
+  const int a = 5;
+  const int b = 10;
+  const int c = 2;
+  const int d = 10;
+  const int n = 100;
+  const double res = 1406.25;
+  func fuction = x_mul_y;
+
+  // Create data
+  std::vector<int> in = {a, b, c, d, n};
+  std::vector<double> out(1, res);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(fuction));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  TestTaskSequentialSimpson testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_LE(res-out[0], 0.001);
+}
+
+TEST(Sequential_ivanov, sin_cos_0_2_0_1) {
+  const int a = 0;
+  const int b = 2;
+  const int c = 0;
+  const int d = 1;
+  const int n = 100;
+  const double res = 1.828692;
+  func fuction = sin_cos;
+
+  // Create data
+  std::vector<int> in = {a, b, c, d, n};
+  std::vector<double> out(1, res);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(fuction));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  TestTaskSequentialSimpson testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_LE(res-out[0], 0.001);
+}
+
+TEST(Sequential_ivanov, liner_10_12_2_4) {
+  const int a = 10;
+  const int b = 12;
+  const int c = 2;
+  const int d = 4;
+  const int n = 100;
+  const double res = 56.0;
+  func fuction = linear_fun;
+
+  // Create data
+  std::vector<int> in = {a, b, c, d, n};
+  std::vector<double> out(1, res);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(fuction));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  TestTaskSequentialSimpson testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_LE(res-out[0], 0.001);
+}
+
+TEST(Sequential_ivanov, Test_Sum_100) {
+  const int a = 10;
+  const int b = 12;
+  const int c = 2;
+  const int d = 4;
+  const int n = 100;
+  const double res = 132.0;
+  func fuction = x_mul_y;
+
+  // Create data
+  std::vector<int> in = {a, b, c, d, n};
+  std::vector<double> out(1, res);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(fuction));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  TestTaskSequentialSimpson testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_LE(res-out[0], 0.001);
+}
---
+++ tasks/seq/ivanov_n_int_simpson/include/ops_seq.hpp
@@ -0,0 +1,26 @@
+// Copyright 2023 Nesterov Alexander
+#pragma once
+
+#include <string>
+#include <vector>
+#include "core/task/include/task.hpp"
+
+typedef double (*func)(double a, double b);
+
+class TestTaskSequentialSimpson : public ppc::core::Task {
+ public:
+  explicit TestTaskSequentialSimpson(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int a{}, b{}, c{}, d{}, n{};
+  double res{};
+  func fun;
+};
+
+double linear_fun(double x, double y);
+double sin_cos(double x, double y);
+double x_mul_y(double x, double y);
