9c3428d10ff1c07f873b632567f4839df21e49c2
Danila <alexseev_dnl@mail.ru>
2024-03-23 01:06:32 +0300

    done

---
+++ tasks/seq/alexseev_d_hoare_sort_w_batcher_merge/include/hoare_sort_w_batcher_merge_seq.hpp
@@ -0,0 +1,23 @@
+// Copyright 2024 Alexseev Danila
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class HoareSortWBatcherMergeSequential : public ppc::core::Task {
+ public:
+  explicit HoareSortWBatcherMergeSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+  void hoareSortWBatcherMergeSeq(std::vector<int> &arr, size_t l, size_t r);
+  void compexch(int &a, int &b);
+
+ private:
+  std::vector<int> array{};
+};
+
+std::vector<int> generateRandomVector(int size, int minVal, int maxVal);
