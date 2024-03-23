fee30b3c600afdb6ecaed83fccae3d29ca0108f6
Zhenya_Kulaev <kylaev11@gmail.com>
2024-03-24 02:09:02 +0300

    first commit

---
+++ tasks/seq/kulaev_e_block_cannons/include/ops_seq.hpp
@@ -0,0 +1,60 @@
+// Copyright 2024 Kulaev Zhenya
+#pragma once
+
+#include <string>
+#include <vector>
+#include <random>
+#include <memory>
+#include <utility>
+
+#include "core/task/include/task.hpp"
+
+class TestTaskSequentialCannon : public ppc::core::Task {
+ public:
+  explicit TestTaskSequentialCannon
+  (std::shared_ptr<ppc::core::TaskData> taskData_)
+  : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> A;
+  std::vector<double> B;
+  std::vector<double> result;
+  int n = 0, m = 0;
+};
+
+inline std::vector<double> getRandomMatrix(int rows, int cols) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<double> dis(1.0, 20.0);
+
+  std::vector<double> matrix(rows * cols);
+
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      matrix[i * cols + j] = dis(gen);
+    }
+  }
+
+  return matrix;
+}
+
+inline std::vector<double> multiplyBlocks(const std::vector<double>& A,
+          const std::vector<double>& B,
+          int rows_A,
+          int col_B) {
+  int col_A = rows_A;
+  std::vector<double> C(rows_A * col_B, 0.0);
+
+  for (int i = 0; i < rows_A; ++i) {
+      for (int j = 0; j < col_B; ++j) {
+          for (int k = 0; k < col_A; ++k) {
+            C[i * col_B + j] += A[i * col_A + k] * B[k * col_B + j];
+          }
+      }
+  }
+  return C;
+}
---
+++ tasks/seq/kulaev_e_block_cannons/src/ops_seq.cpp
@@ -0,0 +1,93 @@
+// Copyright 2024 Kulaev Zhenya
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+#include "seq/kulaev_e_block_cannons/include/ops_seq.hpp"
+
+std::vector<double> cannonMatrixMultiplication(const std::vector<double>& A,
+            const std::vector<double>& B,
+                  int n,
+                  int m) {
+    int blockSize = std::min({n, m});
+
+    std::vector<double> C(n * m, 0.0);
+
+    for (int i = 0; i < n; i += blockSize) {
+        for (int j = 0; j < m; j += blockSize) {
+            for (int k = 0; k < m; k += blockSize) {
+                int i_end = std::min(i + blockSize, n);
+                int j_end = std::min(j + blockSize, m);
+                int k_end = std::min(k + blockSize, m);
+
+                for (int ii = i; ii < i_end; ++ii) {
+                    for (int kk = k; kk < k_end; ++kk) {
+                        double A_ik = A[ii * m + kk];
+                        for (int jj = j; jj < j_end; ++jj) {
+                            C[ii * m + jj] += A_ik * B[kk * m + jj];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return C;
+}
+
+
+bool TestTaskSequentialCannon::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+  // std::cout<<"pre_processing1\n";
+
+  A = std::vector<double>(taskData->inputs_count[0]);
+  B = std::vector<double>(taskData->inputs_count[1]);
+
+  n = *reinterpret_cast<int*>(taskData->inputs[2]);
+  m = *reinterpret_cast<int*>(taskData->inputs[3]);
+
+  //  std::cout << n << std::endl;
+  //  std::cout << m << std::endl;
+
+  //  result = std::vector<double>(n * m, 0);
+
+  //  std::cout <<"pre_processing2\n";
+
+  auto* tmp_ptr_A = reinterpret_cast<double*>(taskData->inputs[0]);
+  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+    A[i] = tmp_ptr_A[i];
+  }
+
+  auto* tmp_ptr_B = reinterpret_cast<double*>(taskData->inputs[1]);
+  for (unsigned i = 0; i < taskData->inputs_count[1]; i++) {
+    B[i] = tmp_ptr_B[i];
+  }
+  //  std::cout<<"pre_processing3\n";
+  return true;
+}
+
+bool TestTaskSequentialCannon::validation() {
+  internal_order_test();
+  // Check count elements of output
+  //  std::cout<<"validation\n";
+  return taskData->inputs_count[0] == taskData->inputs_count[1] &&
+      taskData->inputs_count[0] == taskData->outputs_count[0] &&
+      taskData->inputs_count[1] == taskData->outputs_count[0];
+}
+
+bool TestTaskSequentialCannon::run() {
+  internal_order_test();
+  //  std::cout<<"run do\n";
+  result = cannonMatrixMultiplication(A, B, n, m);
+  //  std::cout<<"run after\n";
+  return true;
+}
+
+bool TestTaskSequentialCannon::post_processing() {
+  internal_order_test();
+  std::copy(result.begin(), result.end(),
+            reinterpret_cast<double*>(taskData->outputs[0]));
+  return true;
+}
