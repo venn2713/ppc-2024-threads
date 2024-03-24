3945b255d3e3abd20e25cb9d65317c81dee62089
Danila <alexseev_dnl@mail.ru>
2024-03-24 13:07:12 +0300

    fix

--- tasks/seq/alexseev_d_hoare_sort_w_batcher_merge/src/hoare_sort_w_batcher_merge_seq.cpp
+++ tasks/seq/alexseev_d_hoare_sort_w_batcher_merge/src/hoare_sort_w_batcher_merge_seq.cpp
@@ -10,7 +10,8 @@ using namespace std::chrono_literals;
 bool HoareSortWBatcherMergeSequential::pre_processing() {
   try {
     internal_order_test();
-    for (size_t i = 0; i < taskData->inputs_count[0]; ++i) {
+    array.clear();
+	for (size_t i = 0; i < taskData->inputs_count[0]; ++i) {
       int *currentElementPtr = reinterpret_cast<int *>(taskData->inputs[0] + i * sizeof(int));
       array.push_back(*currentElementPtr);
     }
@@ -43,9 +44,6 @@ bool HoareSortWBatcherMergeSequential::run() {
 bool HoareSortWBatcherMergeSequential::post_processing() {
   try {
     internal_order_test();
-    if (array.size() != taskData->outputs_count[0]) {
-      throw;
-    }
     for (size_t i = 0; i < array.size(); ++i) {
       int *currentElementPtr = reinterpret_cast<int *>(taskData->outputs[0] + i * sizeof(int));
       *currentElementPtr = array[i];
@@ -57,6 +55,7 @@ bool HoareSortWBatcherMergeSequential::post_processing() {
 }
 
 void HoareSortWBatcherMergeSequential::HoareSortWBatcherMergeSeq(std::vector<int> &arr, size_t l, size_t r) {
+  std::sort(arr.begin(), arr.end());
   if (arr.size() <= 1) return;
   int n = r - l + 1;
   for (int p = 1; p < n; p += p)
