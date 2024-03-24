30954e54341d931184b7999da96e11e08a59eea1
Zhenya_Kulaev <kylaev11@gmail.com>
2024-03-24 03:34:11 +0300

    fix error

--- tasks/seq/kulaev_e_block_cannons/include/ops_seq.hpp
+++ tasks/seq/kulaev_e_block_cannons/include/ops_seq.hpp
@@ -55,30 +55,3 @@ inline std::vector<double> multiplyMatrix(const std::vector<double>& A, const st
   return C;
 }
 
-inline std::vector<double> cannonMatrixMultiplication(const std::vector<double>& A, const std::vector<double>& B, int n,
-                                                      int m) {
-  int blockSize = std::min({n, m});
-
-  std::vector<double> C(n * m, 0.0);
-
-  for (int i = 0; i < n; i += blockSize) {
-    for (int j = 0; j < m; j += blockSize) {
-      for (int k = 0; k < m; k += blockSize) {
-        int i_end = std::min(i + blockSize, n);
-        int j_end = std::min(j + blockSize, m);
-        int k_end = std::min(k + blockSize, m);
-
-        for (int ii = i; ii < i_end; ++ii) {
-          for (int kk = k; kk < k_end; ++kk) {
-            double A_ik = A[ii * m + kk];
-            for (int jj = j; jj < j_end; ++jj) {
-              C[ii * m + jj] += A_ik * B[kk * m + jj];
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return C;
-}
