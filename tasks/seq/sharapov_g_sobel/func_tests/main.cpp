b7a9a83c209c8d5e88660aff061be939e7d12f8c
nol0n <sharapovegor2003@gmail.com>
2024-03-27 04:43:11 +0300

    define to enum and style

--- tasks/seq/sharapov_g_sobel/func_tests/main.cpp
+++ tasks/seq/sharapov_g_sobel/func_tests/main.cpp
@@ -7,196 +7,181 @@
 #include "seq/sharapov_g_sobel/include/ssobel_seq.hpp"
 
 TEST(sharapov_g_sobel_seq, Test_Seed_1234) {
-    int width = 10;
-    int height = 10;
-    std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 1234);
-    std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
-    std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
-        {255}, {0}, {255}, {255}, {255}, {0}, {255}, {0}, {255}, {0},
-        {255}, {0}, {0}, {0}, {0}, {0}, {255}, {255}, {255}, {255},
-        {255}, {255}, {0}, {0}, {0}, {255}, {0}, {255}, {0}, {0},
-        {255}, {0}, {0}, {0}, {0}, {0}, {255}, {0}, {255}, {255},
-        {255}, {255}, {0}, {0}, {255}, {255}, {255}, {0}, {255}, {0},
-        {0}, {0}, {0}, {0}, {0}, {0}, {255}, {255}, {255}, {255},
-        {255}, {255}, {0}, {0}, {0}, {0}, {255}, {255}, {0}, {0},
-        {255}, {255}, {0}, {0}, {0}, {0}, {0}, {255}, {255}, {255},
-        {0}, {255}, {255}, {255}, {0}, {0}, {0}, {0}, {0}, {0},
-        {255}, {255}, {255}, {255}, {255}, {255}, {255}, {0}, {255}, {0},
-    };
+  int width = 10;
+  int height = 10;
+  std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 1234);
+  std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
+  std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
+      {255}, {0},   {255}, {255}, {255}, {0},   {255}, {0},   {255}, {0},   {255}, {0},   {0},   {0},   {0},
+      {0},   {255}, {255}, {255}, {255}, {255}, {255}, {0},   {0},   {0},   {255}, {0},   {255}, {0},   {0},
+      {255}, {0},   {0},   {0},   {0},   {0},   {255}, {0},   {255}, {255}, {255}, {255}, {0},   {0},   {255},
+      {255}, {255}, {0},   {255}, {0},   {0},   {0},   {0},   {0},   {0},   {0},   {255}, {255}, {255}, {255},
+      {255}, {255}, {0},   {0},   {0},   {0},   {255}, {255}, {0},   {0},   {255}, {255}, {0},   {0},   {0},
+      {0},   {0},   {255}, {255}, {255}, {0},   {255}, {255}, {255}, {0},   {0},   {0},   {0},   {0},   {0},
+      {255}, {255}, {255}, {255}, {255}, {255}, {255}, {0},   {255}, {0},
+  };
 
-    // TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
-    taskDataSeq->inputs_count.emplace_back(width);
-    taskDataSeq->inputs_count.emplace_back(height);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
-    taskDataSeq->outputs_count.emplace_back(width);
-    taskDataSeq->outputs_count.emplace_back(height);
+  // TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
+  taskDataSeq->inputs_count.emplace_back(width);
+  taskDataSeq->inputs_count.emplace_back(height);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
+  taskDataSeq->outputs_count.emplace_back(width);
+  taskDataSeq->outputs_count.emplace_back(height);
 
-    // Task
-    SSobelSeq SSobelSeq(taskDataSeq);
-    ASSERT_EQ(SSobelSeq.validation(), true);
-    ASSERT_EQ(SSobelSeq.pre_processing(), true);
-    ASSERT_EQ(SSobelSeq.run(), true);
-    ASSERT_EQ(SSobelSeq.post_processing(), true);
+  // Task
+  SSobelSeq SSobelSeq(taskDataSeq);
+  ASSERT_EQ(SSobelSeq.validation(), true);
+  ASSERT_EQ(SSobelSeq.pre_processing(), true);
+  ASSERT_EQ(SSobelSeq.run(), true);
+  ASSERT_EQ(SSobelSeq.post_processing(), true);
 
-    for (size_t i = 0; i < ColoredImage.size(); ++i) {
-        ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
-    }
+  for (size_t i = 0; i < ColoredImage.size(); ++i) {
+    ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
+  }
 }
 
 TEST(sharapov_g_sobel_seq, Test_Seed_2451) {
-    int width = 10;
-    int height = 10;
-    std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2451);
-    std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
-    std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
-        {0}, {255}, {255}, {0}, {255}, {255}, {0}, {0}, {0}, {0},
-        {255}, {0}, {0}, {255}, {255}, {255}, {0}, {0}, {0}, {255},
-        {0}, {255}, {255}, {255}, {0}, {255}, {255}, {255}, {255}, {255},
-        {0}, {0}, {0}, {0}, {255}, {255}, {255}, {0}, {0}, {255},
-        {0}, {0}, {255}, {255}, {0}, {255}, {255}, {255}, {255}, {255},
-        {255}, {255}, {0}, {255}, {0}, {255}, {0}, {255}, {255}, {255},
-        {255}, {0}, {255}, {0}, {255}, {255}, {255}, {255}, {255}, {0},
-        {255}, {255}, {255}, {0}, {255}, {255}, {255}, {255}, {255}, {255},
-        {255}, {255}, {0}, {255}, {255}, {255}, {0}, {255}, {255}, {255},
-        {255}, {255}, {255}, {0}, {255}, {255}, {255}, {255}, {0}, {255},
-    };
+  int width = 10;
+  int height = 10;
+  std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2451);
+  std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
+  std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
+      {0},   {255}, {255}, {0},   {255}, {255}, {0},   {0},   {0},   {0},   {255}, {0},   {0},   {255}, {255},
+      {255}, {0},   {0},   {0},   {255}, {0},   {255}, {255}, {255}, {0},   {255}, {255}, {255}, {255}, {255},
+      {0},   {0},   {0},   {0},   {255}, {255}, {255}, {0},   {0},   {255}, {0},   {0},   {255}, {255}, {0},
+      {255}, {255}, {255}, {255}, {255}, {255}, {255}, {0},   {255}, {0},   {255}, {0},   {255}, {255}, {255},
+      {255}, {0},   {255}, {0},   {255}, {255}, {255}, {255}, {255}, {0},   {255}, {255}, {255}, {0},   {255},
+      {255}, {255}, {255}, {255}, {255}, {255}, {255}, {0},   {255}, {255}, {255}, {0},   {255}, {255}, {255},
+      {255}, {255}, {255}, {0},   {255}, {255}, {255}, {255}, {0},   {255},
+  };
 
-    // TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
-    taskDataSeq->inputs_count.emplace_back(width);
-    taskDataSeq->inputs_count.emplace_back(height);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
-    taskDataSeq->outputs_count.emplace_back(width);
-    taskDataSeq->outputs_count.emplace_back(height);
+  // TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
+  taskDataSeq->inputs_count.emplace_back(width);
+  taskDataSeq->inputs_count.emplace_back(height);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
+  taskDataSeq->outputs_count.emplace_back(width);
+  taskDataSeq->outputs_count.emplace_back(height);
 
-    // Task
-    SSobelSeq SSobelSeq(taskDataSeq);
-    ASSERT_EQ(SSobelSeq.validation(), true);
-    ASSERT_EQ(SSobelSeq.pre_processing(), true);
-    ASSERT_EQ(SSobelSeq.run(), true);
-    ASSERT_EQ(SSobelSeq.post_processing(), true);
+  // Task
+  SSobelSeq SSobelSeq(taskDataSeq);
+  ASSERT_EQ(SSobelSeq.validation(), true);
+  ASSERT_EQ(SSobelSeq.pre_processing(), true);
+  ASSERT_EQ(SSobelSeq.run(), true);
+  ASSERT_EQ(SSobelSeq.post_processing(), true);
 
-    for (size_t i = 0; i < ColoredImage.size(); ++i) {
-        ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
-    }
+  for (size_t i = 0; i < ColoredImage.size(); ++i) {
+    ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
+  }
 }
 
 TEST(sharapov_g_sobel_seq, Test_Seed_2423) {
-    int width = 10;
-    int height = 10;
-    std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2423);
-    std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
-    std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
-        {255}, {255}, {255}, {0}, {0}, {255}, {255}, {255}, {0}, {0},
-        {0}, {255}, {0}, {255}, {255}, {0}, {0}, {0}, {0}, {0},
-        {0}, {255}, {255}, {0}, {0}, {255}, {255}, {0}, {0}, {0},
-        {255}, {255}, {0}, {255}, {255}, {255}, {0}, {0}, {0}, {255},
-        {0}, {0}, {0}, {0}, {255}, {0}, {0}, {0}, {255}, {255},
-        {255}, {255}, {255}, {0}, {0}, {255}, {255}, {0}, {0}, {255},
-        {255}, {255}, {0}, {255}, {255}, {0}, {255}, {255}, {255}, {0},
-        {0}, {255}, {0}, {0}, {255}, {0}, {0}, {0}, {255}, {0},
-        {255}, {255}, {255}, {0}, {0}, {0}, {0}, {0}, {255}, {255},
-        {255}, {255}, {0}, {0}, {0}, {0}, {255}, {255}, {255}, {0},
-    };
+  int width = 10;
+  int height = 10;
+  std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2423);
+  std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
+  std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
+      {255}, {255}, {255}, {0},   {0},   {255}, {255}, {255}, {0},   {0},   {0},   {255}, {0}, {255}, {255},
+      {0},   {0},   {0},   {0},   {0},   {0},   {255}, {255}, {0},   {0},   {255}, {255}, {0}, {0},   {0},
+      {255}, {255}, {0},   {255}, {255}, {255}, {0},   {0},   {0},   {255}, {0},   {0},   {0}, {0},   {255},
+      {0},   {0},   {0},   {255}, {255}, {255}, {255}, {255}, {0},   {0},   {255}, {255}, {0}, {0},   {255},
+      {255}, {255}, {0},   {255}, {255}, {0},   {255}, {255}, {255}, {0},   {0},   {255}, {0}, {0},   {255},
+      {0},   {0},   {0},   {255}, {0},   {255}, {255}, {255}, {0},   {0},   {0},   {0},   {0}, {255}, {255},
+      {255}, {255}, {0},   {0},   {0},   {0},   {255}, {255}, {255}, {0},
+  };
 
-    // TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
-    taskDataSeq->inputs_count.emplace_back(width);
-    taskDataSeq->inputs_count.emplace_back(height);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
-    taskDataSeq->outputs_count.emplace_back(width);
-    taskDataSeq->outputs_count.emplace_back(height);
+  // TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
+  taskDataSeq->inputs_count.emplace_back(width);
+  taskDataSeq->inputs_count.emplace_back(height);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
+  taskDataSeq->outputs_count.emplace_back(width);
+  taskDataSeq->outputs_count.emplace_back(height);
 
-    // Task
-    SSobelSeq SSobelSeq(taskDataSeq);
-    ASSERT_EQ(SSobelSeq.validation(), true);
-    ASSERT_EQ(SSobelSeq.pre_processing(), true);
-    ASSERT_EQ(SSobelSeq.run(), true);
-    ASSERT_EQ(SSobelSeq.post_processing(), true);
+  // Task
+  SSobelSeq SSobelSeq(taskDataSeq);
+  ASSERT_EQ(SSobelSeq.validation(), true);
+  ASSERT_EQ(SSobelSeq.pre_processing(), true);
+  ASSERT_EQ(SSobelSeq.run(), true);
+  ASSERT_EQ(SSobelSeq.post_processing(), true);
 
-    for (size_t i = 0; i < ColoredImage.size(); ++i) {
-        ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
-    }
+  for (size_t i = 0; i < ColoredImage.size(); ++i) {
+    ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
+  }
 }
 
 TEST(sharapov_g_sobel_seq, Test_Seed_2354) {
-    int width = 10;
-    int height = 10;
-    std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2354);
-    std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
-    std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
-        {255}, {255}, {0}, {255}, {0}, {0}, {0}, {0}, {0}, {0},
-        {255}, {255}, {255}, {255}, {0}, {255}, {0}, {0}, {255}, {0},
-        {255}, {255}, {0}, {0}, {0}, {255}, {255}, {0}, {255}, {0},
-        {255}, {255}, {0}, {0}, {255}, {255}, {255}, {0}, {255}, {0},
-        {255}, {255}, {0}, {255}, {255}, {0}, {255}, {0}, {0}, {255},
-        {0}, {255}, {255}, {0}, {0}, {255}, {255}, {255}, {0}, {0},
-        {255}, {255}, {255}, {255}, {0}, {255}, {0}, {255}, {255}, {255},
-        {0}, {255}, {0}, {255}, {0}, {255}, {255}, {255}, {0}, {0},
-        {255}, {0}, {255}, {255}, {255}, {0}, {255}, {255}, {0}, {0},
-        {0}, {255}, {255}, {255}, {255}, {255}, {0}, {255}, {0}, {0},
-    };
+  int width = 10;
+  int height = 10;
+  std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 2354);
+  std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
+  std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
+      {255}, {255}, {0},   {255}, {0},   {0},   {0},   {0},   {0},   {0},   {255}, {255}, {255}, {255}, {0},
+      {255}, {0},   {0},   {255}, {0},   {255}, {255}, {0},   {0},   {0},   {255}, {255}, {0},   {255}, {0},
+      {255}, {255}, {0},   {0},   {255}, {255}, {255}, {0},   {255}, {0},   {255}, {255}, {0},   {255}, {255},
+      {0},   {255}, {0},   {0},   {255}, {0},   {255}, {255}, {0},   {0},   {255}, {255}, {255}, {0},   {0},
+      {255}, {255}, {255}, {255}, {0},   {255}, {0},   {255}, {255}, {255}, {0},   {255}, {0},   {255}, {0},
+      {255}, {255}, {255}, {0},   {0},   {255}, {0},   {255}, {255}, {255}, {0},   {255}, {255}, {0},   {0},
+      {0},   {255}, {255}, {255}, {255}, {255}, {0},   {255}, {0},   {0},
+  };
 
-    // TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
-    taskDataSeq->inputs_count.emplace_back(width);
-    taskDataSeq->inputs_count.emplace_back(height);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
-    taskDataSeq->outputs_count.emplace_back(width);
-    taskDataSeq->outputs_count.emplace_back(height);
+  // TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
+  taskDataSeq->inputs_count.emplace_back(width);
+  taskDataSeq->inputs_count.emplace_back(height);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
+  taskDataSeq->outputs_count.emplace_back(width);
+  taskDataSeq->outputs_count.emplace_back(height);
 
-    // Task
-    SSobelSeq SSobelSeq(taskDataSeq);
-    ASSERT_EQ(SSobelSeq.validation(), true);
-    ASSERT_EQ(SSobelSeq.pre_processing(), true);
-    ASSERT_EQ(SSobelSeq.run(), true);
-    ASSERT_EQ(SSobelSeq.post_processing(), true);
+  // Task
+  SSobelSeq SSobelSeq(taskDataSeq);
+  ASSERT_EQ(SSobelSeq.validation(), true);
+  ASSERT_EQ(SSobelSeq.pre_processing(), true);
+  ASSERT_EQ(SSobelSeq.run(), true);
+  ASSERT_EQ(SSobelSeq.post_processing(), true);
 
-    for (size_t i = 0; i < ColoredImage.size(); ++i) {
-        ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
-    }
+  for (size_t i = 0; i < ColoredImage.size(); ++i) {
+    ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
+  }
 }
 
 TEST(sharapov_g_sobel_seq, Test_Seed_3453) {
-    int width = 10;
-    int height = 10;
-    std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 3453);
-    std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
-    std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
-        {0}, {255}, {0}, {0}, {0}, {255}, {0}, {255}, {0}, {0},
-        {255}, {255}, {0}, {0}, {0}, {255}, {0}, {255}, {0}, {255},
-        {0}, {0}, {0}, {0}, {0}, {0}, {255}, {255}, {0}, {0},
-        {0}, {0}, {0}, {255}, {255}, {0}, {0}, {0}, {255}, {0},
-        {0}, {255}, {255}, {255}, {255}, {0}, {0}, {0}, {0}, {0},
-        {255}, {255}, {0}, {255}, {0}, {255}, {255}, {0}, {0}, {0},
-        {255}, {255}, {255}, {255}, {255}, {255}, {0}, {0}, {0}, {0},
-        {0}, {255}, {255}, {255}, {0}, {0}, {0}, {0}, {0}, {0},
-        {255}, {0}, {255}, {255}, {255}, {0}, {0}, {255}, {0}, {255},
-        {255}, {0}, {0}, {0}, {255}, {0}, {255}, {255}, {255}, {255},
-    };
+  int width = 10;
+  int height = 10;
+  std::vector<SSobelSeq::RGB> ColoredImage = SSobelSeq::generateColorImage(width, height, 3453);
+  std::vector<SSobelSeq::GrayScale> EdgeImage(ColoredImage.size());
+  std::vector<SSobelSeq::GrayScale> EdgeImageExpected = {
+      {0},   {255}, {0},   {0},   {0},   {255}, {0},   {255}, {0},   {0},   {255}, {255}, {0},   {0},   {0},
+      {255}, {0},   {255}, {0},   {255}, {0},   {0},   {0},   {0},   {0},   {0},   {255}, {255}, {0},   {0},
+      {0},   {0},   {0},   {255}, {255}, {0},   {0},   {0},   {255}, {0},   {0},   {255}, {255}, {255}, {255},
+      {0},   {0},   {0},   {0},   {0},   {255}, {255}, {0},   {255}, {0},   {255}, {255}, {0},   {0},   {0},
+      {255}, {255}, {255}, {255}, {255}, {255}, {0},   {0},   {0},   {0},   {0},   {255}, {255}, {255}, {0},
+      {0},   {0},   {0},   {0},   {0},   {255}, {0},   {255}, {255}, {255}, {0},   {0},   {255}, {0},   {255},
+      {255}, {0},   {0},   {0},   {255}, {0},   {255}, {255}, {255}, {255},
+  };
 
-    // TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
-    taskDataSeq->inputs_count.emplace_back(width);
-    taskDataSeq->inputs_count.emplace_back(height);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
-    taskDataSeq->outputs_count.emplace_back(width);
-    taskDataSeq->outputs_count.emplace_back(height);
+  // TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(ColoredImage.data()));
+  taskDataSeq->inputs_count.emplace_back(width);
+  taskDataSeq->inputs_count.emplace_back(height);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(EdgeImage.data()));
+  taskDataSeq->outputs_count.emplace_back(width);
+  taskDataSeq->outputs_count.emplace_back(height);
 
-    // Task
-    SSobelSeq SSobelSeq(taskDataSeq);
-    ASSERT_EQ(SSobelSeq.validation(), true);
-    ASSERT_EQ(SSobelSeq.pre_processing(), true);
-    ASSERT_EQ(SSobelSeq.run(), true);
-    ASSERT_EQ(SSobelSeq.post_processing(), true);
+  // Task
+  SSobelSeq SSobelSeq(taskDataSeq);
+  ASSERT_EQ(SSobelSeq.validation(), true);
+  ASSERT_EQ(SSobelSeq.pre_processing(), true);
+  ASSERT_EQ(SSobelSeq.run(), true);
+  ASSERT_EQ(SSobelSeq.post_processing(), true);
 
-    for (size_t i = 0; i < ColoredImage.size(); ++i) {
-        ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
-    }
+  for (size_t i = 0; i < ColoredImage.size(); ++i) {
+    ASSERT_EQ(EdgeImageExpected[i].value, EdgeImage[i].value);
+  }
 }
--- tasks/seq/sharapov_g_sobel/src/ssobel_seq.cpp
+++ tasks/seq/sharapov_g_sobel/src/ssobel_seq.cpp
@@ -1,158 +1,156 @@
 // Copyright 2024 Sharapov Georgiy
 
+#include "seq/sharapov_g_sobel/include/ssobel_seq.hpp"
+
 #include <cmath>
 #include <random>
 #include <thread>
-#include "seq/sharapov_g_sobel/include/ssobel_seq.hpp"
 
-SSobelSeq::GrayScale SSobelSeq::getPixel(const std::vector<SSobelSeq::GrayScale>& image,
-                    size_t x, size_t y, size_t width, size_t height) {
-    if (x > width - 1) x = width - 1;
-    if (x < 0) x = 0;
-    if (y > height - 1) y = height - 1;
-    if (y < 0) y = 0;
+SSobelSeq::GrayScale SSobelSeq::getPixel(const std::vector<SSobelSeq::GrayScale>& image, size_t x, size_t y,
+                                         size_t width, size_t height) {
+  if (x > width - 1) x = width - 1;
+  if (x < 0) x = 0;
+  if (y > height - 1) y = height - 1;
+  if (y < 0) y = 0;
 
-    return image[y * width + x];
+  return image[y * width + x];
 }
 
 std::vector<SSobelSeq::RGB> SSobelSeq::generateColorImage(size_t width, size_t height, size_t seed) {
-    std::vector<SSobelSeq::RGB> image;
-    image.reserve(width * height);
+  std::vector<SSobelSeq::RGB> image;
+  image.reserve(width * height);
 
-    std::random_device rd;
-    std::mt19937 gen(static_cast<unsigned int>(seed));
-    std::uniform_int_distribution<> rgb(0, 255);
+  std::random_device rd;
+  std::mt19937 gen(static_cast<unsigned int>(seed));
+  std::uniform_int_distribution<> rgb(0, 255);
 
-    for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            SSobelSeq::RGB pixel;
-            pixel.r = static_cast<uint8_t>(rgb(gen));
-            pixel.g = static_cast<uint8_t>(rgb(gen));
-            pixel.b = static_cast<uint8_t>(rgb(gen));
-            image.push_back(pixel);
-        }
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      SSobelSeq::RGB pixel;
+      pixel.r = static_cast<uint8_t>(rgb(gen));
+      pixel.g = static_cast<uint8_t>(rgb(gen));
+      pixel.b = static_cast<uint8_t>(rgb(gen));
+      image.push_back(pixel);
     }
+  }
 
-    return image;
+  return image;
 }
 
 std::vector<SSobelSeq::GrayScale> SSobelSeq::convertToGrayScale(const std::vector<SSobelSeq::RGB>& colorImage,
-                                            size_t width, size_t height) {
-    std::vector<SSobelSeq::GrayScale> grayImage(width * height);
-    for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            const auto& pixel = colorImage[i * width + j];
-            grayImage[i * width + j].value = static_cast<uint8_t>(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b);
-        }
+                                                                size_t width, size_t height) {
+  std::vector<SSobelSeq::GrayScale> grayImage(width * height);
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      const auto& pixel = colorImage[i * width + j];
+      grayImage[i * width + j].value = static_cast<uint8_t>(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b);
     }
-    return grayImage;
+  }
+  return grayImage;
 }
 
 std::vector<SSobelSeq::GrayScale> SSobelSeq::SobelOperatorSeq(const std::vector<SSobelSeq::GrayScale>& grayImage,
-                                            size_t width, size_t height) {
-    const int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    const int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+                                                              size_t width, size_t height) {
+  const int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+  const int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
-    std::vector<GrayScale> result(width * height);
+  std::vector<GrayScale> result(width * height);
 
-    for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            int sumX = 0, sumY = 0;
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      int sumX = 0, sumY = 0;
 
-            for (int x = -1; x <= 1; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    int posX = static_cast<int>(j) + y;
-                    int posY = static_cast<int>(i) + x;
-                    auto pixel = getPixel(grayImage, posX, posY, width, height);
+      for (int x = -1; x <= 1; x++) {
+        for (int y = -1; y <= 1; y++) {
+          int posX = static_cast<int>(j) + y;
+          int posY = static_cast<int>(i) + x;
+          auto pixel = getPixel(grayImage, posX, posY, width, height);
 
-                    sumX += Gx[x + 1][y + 1] * pixel.value;
-                    sumY += Gy[x + 1][y + 1] * pixel.value;
-                }
-            }
-
-            int sum = std::sqrt(sumX * sumX + sumY * sumY);
-
-            if (sum >= 200) {
-                sum = 255;
-            } else {
-                sum = 0;
-            }
-
-            result[i * width + j] = GrayScale{static_cast<uint8_t>(sum)};
+          sumX += Gx[x + 1][y + 1] * pixel.value;
+          sumY += Gy[x + 1][y + 1] * pixel.value;
         }
-    }
+      }
 
-    return result;
+      int sum = std::sqrt(sumX * sumX + sumY * sumY);
+
+      if (sum >= 200) {
+        sum = 255;
+      } else {
+        sum = 0;
+      }
+
+      result[i * width + j] = GrayScale{static_cast<uint8_t>(sum)};
+    }
+  }
+
+  return result;
 }
 
 void SSobelSeq::printPixel(std::vector<GrayScale> Image, int width, int height) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            GrayScale pixel = getPixel(Image, x, y, width, height);
-            std::cout << static_cast<int>(pixel.value) << "\t";
-        }
-        std::cout << std::endl;
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      GrayScale pixel = getPixel(Image, x, y, width, height);
+      std::cout << static_cast<int>(pixel.value) << "\t";
     }
     std::cout << std::endl;
+  }
+  std::cout << std::endl;
 }
 
 bool SSobelSeq::validation() {
-    try {
-        internal_order_test();
-    } catch (...) {
-        return false;
-    }
+  try {
+    internal_order_test();
+  } catch (...) {
+    return false;
+  }
 
-    return (taskData->inputs_count[0] == taskData->outputs_count[0] &&
-            taskData->inputs_count[1] == taskData->outputs_count[1]);
+  return (taskData->inputs_count[0] == taskData->outputs_count[0] &&
+          taskData->inputs_count[1] == taskData->outputs_count[1]);
 }
 
 bool SSobelSeq::pre_processing() {
-    try {
-        internal_order_test();
+  try {
+    internal_order_test();
 
-        imgWidth = taskData->inputs_count[0];
-        imgHeight = taskData->inputs_count[1];
-        imgSize = imgWidth * imgHeight;
+    imgWidth = taskData->inputs_count[0];
+    imgHeight = taskData->inputs_count[1];
+    imgSize = imgWidth * imgHeight;
 
-        colored_img.reserve(imgSize);
-        uint8_t* rawData = taskData->inputs[0];
-        for (size_t i = 0; i < imgSize; ++i) {
-            colored_img.push_back(RGB{rawData[i * 3],
-                                      rawData[i * 3 + 1],
-                                      rawData[i * 3 + 2]});
-        }
-
-        grayscale_img = SSobelSeq::convertToGrayScale(colored_img, imgWidth, imgHeight);
-    } catch (...) {
-        return false;
+    colored_img.reserve(imgSize);
+    uint8_t* rawData = taskData->inputs[0];
+    for (size_t i = 0; i < imgSize; ++i) {
+      colored_img.push_back(RGB{rawData[i * 3], rawData[i * 3 + 1], rawData[i * 3 + 2]});
     }
-    return true;
+
+    grayscale_img = SSobelSeq::convertToGrayScale(colored_img, imgWidth, imgHeight);
+  } catch (...) {
+    return false;
+  }
+  return true;
 }
 
 bool SSobelSeq::run() {
-    try {
-        internal_order_test();
+  try {
+    internal_order_test();
 
-        result = SSobelSeq::SobelOperatorSeq(grayscale_img, imgWidth, imgHeight);
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
-    } catch (...) {
-        return false;
-    }
-    return true;
+    result = SSobelSeq::SobelOperatorSeq(grayscale_img, imgWidth, imgHeight);
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  } catch (...) {
+    return false;
+  }
+  return true;
 }
 
 bool SSobelSeq::post_processing() {
-    try {
-        internal_order_test();
+  try {
+    internal_order_test();
 
-        for (size_t i = 0; i < grayscale_img.size(); ++i) {
-            SSobelSeq::GrayScale *pixel = reinterpret_cast<SSobelSeq::GrayScale*>(taskData->outputs[0]
-                                                                + i);
-            *pixel = result[i];
-        }
-    } catch (...) {
-        return false;
+    for (size_t i = 0; i < grayscale_img.size(); ++i) {
+      SSobelSeq::GrayScale* pixel = reinterpret_cast<SSobelSeq::GrayScale*>(taskData->outputs[0] + i);
+      *pixel = result[i];
     }
-    return true;
+  } catch (...) {
+    return false;
+  }
+  return true;
 }
