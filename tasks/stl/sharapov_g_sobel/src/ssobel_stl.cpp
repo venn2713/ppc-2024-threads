25dd4746edaf8cfb9df5b3d64fe79072bcc5b7d9
nol0n <sharapovegor2003@gmail.com>
2024-03-28 22:45:45 +0300

    inital commit

---
+++ tasks/stl/sharapov_g_sobel/src/ssobel_stl.cpp
@@ -0,0 +1,158 @@
+// Copyright 2024 Sharapov Georgiy
+
+#include "stl/sharapov_g_sobel/include/ssobel_stl.hpp"
+
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+SSobelStl::GrayScale SSobelStl::getPixel(const std::vector<SSobelStl::GrayScale>& image, size_t x, size_t y,
+                                         size_t width, size_t height) {
+  if (x > width - 1) x = width - 1;
+  if (x < 0) x = 0;
+  if (y > height - 1) y = height - 1;
+  if (y < 0) y = 0;
+
+  return image[y * width + x];
+}
+
+std::vector<SSobelStl::RGB> SSobelStl::generateColorImage(size_t width, size_t height, size_t seed) {
+  std::vector<SSobelStl::RGB> image;
+  image.reserve(width * height);
+
+  std::mt19937 gen(static_cast<unsigned int>(seed));
+  std::uniform_int_distribution<> rgb(0, 255);
+
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      SSobelStl::RGB pixel;
+      pixel.r = static_cast<uint8_t>(rgb(gen));
+      pixel.g = static_cast<uint8_t>(rgb(gen));
+      pixel.b = static_cast<uint8_t>(rgb(gen));
+      image.push_back(pixel);
+    }
+  }
+
+  return image;
+}
+
+std::vector<SSobelStl::GrayScale> SSobelStl::convertToGrayScale(const std::vector<SSobelStl::RGB>& colorImage,
+                                                                size_t width, size_t height) {
+  std::vector<SSobelStl::GrayScale> grayImage(width * height);
+#pragma Stl parallel for
+  for (size_t index = 0; index < width * height; ++index) {
+    const auto& pixel = colorImage[index];
+    grayImage[index].value = static_cast<uint8_t>(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b);
+  }
+  return grayImage;
+}
+
+std::vector<SSobelStl::GrayScale> SSobelStl::SobelOperatorStl(const std::vector<SSobelStl::GrayScale>& grayImage,
+                                                              size_t width, size_t height) {
+  const int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+  const int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+  std::vector<GrayScale> resultImg(width * height);
+
+  for (size_t index = 0; index < width * height; ++index) {
+    int i = index / width;
+    int j = index % width;
+    int sumX = 0;
+    int sumY = 0;
+
+    for (int x = -1; x <= 1; x++) {
+      for (int y = -1; y <= 1; y++) {
+        int posX = static_cast<int>(j) + y;
+        int posY = static_cast<int>(i) + x;
+        auto pixel = getPixel(grayImage, posX, posY, width, height);
+
+        sumX += Gx[x + 1][y + 1] * pixel.value;
+        sumY += Gy[x + 1][y + 1] * pixel.value;
+      }
+    }
+
+    int sum = std::sqrt(sumX * sumX + sumY * sumY);
+
+    sum = sum >= 200 ? 255 : 0;
+
+    resultImg[index] = GrayScale{static_cast<uint8_t>(sum)};
+  }
+
+  return resultImg;
+}
+
+void SSobelStl::printPixel(const std::vector<GrayScale>& image, int width, int height) {
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      GrayScale pixel = getPixel(image, x, y, width, height);
+      std::cout << static_cast<int>(pixel.value) << "\t";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+bool SSobelStl::validation() {
+  try {
+    internal_order_test();
+  } catch (...) {
+    return false;
+  }
+
+  return (taskData->inputs_count[0] == taskData->outputs_count[0] &&
+          taskData->inputs_count[1] == taskData->outputs_count[1]);
+}
+
+bool SSobelStl::pre_processing() {
+  try {
+    internal_order_test();
+
+    imgWidth = taskData->inputs_count[0];
+    imgHeight = taskData->inputs_count[1];
+    imgSize = imgWidth * imgHeight;
+
+    colored_img.reserve(imgSize);
+    uint8_t* rawData = taskData->inputs[0];
+#pragma Stl parallel for
+    for (size_t i = 0; i < imgSize; ++i) {
+      colored_img[i] = (RGB{rawData[i * 3], rawData[i * 3 + 1], rawData[i * 3 + 2]});
+    }
+
+    grayscale_img = SSobelStl::convertToGrayScale(colored_img, imgWidth, imgHeight);
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+bool SSobelStl::run() {
+  try {
+    internal_order_test();
+
+    result = SSobelStl::SobelOperatorStl(grayscale_img, imgWidth, imgHeight);
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+bool SSobelStl::post_processing() {
+  try {
+    internal_order_test();
+
+    for (size_t i = 0; i < grayscale_img.size(); ++i) {
+      auto* pixel = reinterpret_cast<SSobelStl::GrayScale*>(taskData->outputs[0] + i);
+      *pixel = result[i];
+    }
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
