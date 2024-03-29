212428b9646af962eb8f7ce096462e4669d88422
Khramov Ivan <khramov.vanya@bk.ru>
2024-03-29 23:11:02 +0300

    Third Commit- style fixes

--- tasks/seq/khramov_i_simpson/perf_tests/main.cpp
+++ tasks/seq/khramov_i_simpson/perf_tests/main.cpp
@@ -18,6 +18,7 @@ TEST(khramov_i_simpson_seq, test_pipeline_run) {
   // Create data
   std::vector<int> in{a1, a2, b1, b2, numSteps};
   std::vector<double> out{result};
+  
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
@@ -60,7 +61,6 @@ TEST(khramov_i_simpson_seq, test_task_run) {
   // Create data
   std::vector<int> in{a1, a2, b1, b2, numSteps};
   std::vector<double> out{result};
-
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
