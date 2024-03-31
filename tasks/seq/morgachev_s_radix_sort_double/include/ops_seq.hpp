439c07115d6acace72a9d234fae20b1b89041d86
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-03-31 16:23:39 +0300

    feat: done seq version

---
+++ tasks/seq/morgachev_s_radix_sort_double/include/ops_seq.hpp
@@ -0,0 +1,20 @@
+// Copyright 2024 Morgachev Stepan
+#pragma once
+
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class RadixSortDoubleSequential : public ppc::core::Task {
+ public:
+  explicit RadixSortDoubleSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> data;
+  size_t dataSize;
+  void countSort(double* in, double* out, int length, int exp);
+};
