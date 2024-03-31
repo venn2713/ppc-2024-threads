30817a608db427bba5c3b877347f8f48dc524b87
MineShadow7 <shad-karagodin@ya.ru>
2024-03-31 19:19:42 +0300

    Main sequential algorithm done. Sequential func tests done.

---
+++ tasks/seq/karagodin_a_dejkstra/include/dejkstra_seq.hpp
@@ -0,0 +1,41 @@
+// Copyright 2024 Karagodin Andrey
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <random>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class DejkstraTaskSequential : public ppc::core::Task {
+ public:
+  explicit DejkstraTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+  std::pair<std::vector<int>, int> getDejMinPath(
+    const std::vector<std::vector<int>>& graphMapInput, int entryNode, int destNode);
+  struct Node {
+    int vertex;
+    int cost;
+    Node(int v, int c) : vertex(v), cost(c) {}
+  };
+
+  struct CompareNode {
+    bool operator()(const Node& n1, const Node& n2) { return n1.cost > n2.cost; }
+  };
+  void printGraphMap(std::vector<std::vector<int>>& graphMap);
+
+ private:
+  int size, destNode, entryNode, minScore;
+  std::vector<std::vector<int>> graphMapInput;
+  std::vector<int> pathOutput;
+  std::pair<std::vector<int>, int> res;
+};
+
+std::vector<std::vector<int>> initGraphMapRandom(int16_t size);
