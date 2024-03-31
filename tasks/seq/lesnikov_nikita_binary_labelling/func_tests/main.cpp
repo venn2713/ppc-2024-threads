127a8f6c2b8f1b710c44ee6f39818314b28c7212
nik <lesnikowka@mail.ru>
2024-04-01 01:28:02 +0300

    add 2 test

--- tasks/seq/lesnikov_nikita_binary_labelling/func_tests/main.cpp
+++ tasks/seq/lesnikov_nikita_binary_labelling/func_tests/main.cpp
@@ -45,9 +45,95 @@ TEST(Sequential, ZeroMatrixTest) {
   EXPECT_EQ(BinaryLabellingSequential::deserializeInt32(outNum.data()), expectedObjectsNum);
 }
 
-TEST(SequentialBinaryLabelling, dummy1) { EXPECT_TRUE(true); }
+TEST(SequentialBinaryLabelling, all1Test) {
+  int m = 2;
+  int n = 2;
+  auto serializedM = BinaryLabellingSequential::serializeInt32(m);
+  auto serializedN = BinaryLabellingSequential::serializeInt32(n);
+  std::vector<uint8_t> in = {1, 1, 1, 1};
+  std::vector<uint8_t> outV(in.size());
+  std::vector<uint8_t> outNum(4);
 
-TEST(SequentialBinaryLabelling, dummy2) { EXPECT_TRUE(true); }
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->state_of_testing = ppc::core::TaskData::FUNC;
+  taskDataSeq->inputs.push_back(in.data());
+  taskDataSeq->inputs.push_back(serializedM.data());
+  taskDataSeq->inputs.push_back(serializedN.data());
+  taskDataSeq->inputs_count.push_back(in.size());
+  taskDataSeq->inputs_count.push_back(4);
+  taskDataSeq->inputs_count.push_back(4);
+  taskDataSeq->outputs.push_back(outV.data());
+  taskDataSeq->outputs.push_back(outNum.data());
+  taskDataSeq->outputs_count.push_back(outV.size());
+  taskDataSeq->outputs_count.push_back(4);
+
+  BinaryLabellingSequential testTaskSequential(taskDataSeq);
+  ASSERT_TRUE(testTaskSequential.validation());
+  ASSERT_TRUE(testTaskSequential.pre_processing());
+  ASSERT_TRUE(testTaskSequential.run());
+  ASSERT_TRUE(testTaskSequential.post_processing());
+
+  std::vector<uint8_t> expected = {1, 1, 1, 1};
+  uint32_t expectedObjectsNum = 1;
+
+  std::cout << "obj nums: " << BinaryLabellingSequential::deserializeInt32(outNum.data()) << " " << expectedObjectsNum
+            << std::endl;
+
+  BinaryLabellingSequential::visualize(outV, m, n);
+
+  EXPECT_EQ(outV, expected);
+  EXPECT_EQ(BinaryLabellingSequential::deserializeInt32(outNum.data()), expectedObjectsNum);
+}
+
+TEST(SequentialBinaryLabelling, allSeperatedTest) { 
+     int m = 5;
+  int n = 5;
+  auto serializedM = BinaryLabellingSequential::serializeInt32(m);
+  auto serializedN = BinaryLabellingSequential::serializeInt32(n);
+  std::vector<uint8_t> in = {
+      1, 0, 1, 0, 1,
+      0, 1, 0, 1, 0,
+      1, 0, 1, 0, 1,
+      0, 1, 0, 1, 0,
+      1, 0, 1, 0, 1};
+  std::vector<uint8_t> outV(in.size());
+  std::vector<uint8_t> outNum(4);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->state_of_testing = ppc::core::TaskData::FUNC;
+  taskDataSeq->inputs.push_back(in.data());
+  taskDataSeq->inputs.push_back(serializedM.data());
+  taskDataSeq->inputs.push_back(serializedN.data());
+  taskDataSeq->inputs_count.push_back(in.size());
+  taskDataSeq->inputs_count.push_back(4);
+  taskDataSeq->inputs_count.push_back(4);
+  taskDataSeq->outputs.push_back(outV.data());
+  taskDataSeq->outputs.push_back(outNum.data());
+  taskDataSeq->outputs_count.push_back(outV.size());
+  taskDataSeq->outputs_count.push_back(4);
+
+  BinaryLabellingSequential testTaskSequential(taskDataSeq);
+  ASSERT_TRUE(testTaskSequential.validation());
+  ASSERT_TRUE(testTaskSequential.pre_processing());
+  ASSERT_TRUE(testTaskSequential.run());
+  ASSERT_TRUE(testTaskSequential.post_processing());
+
+  std::vector<uint8_t> expected = {
+      1, 0, 2, 0, 3, 
+      0, 4, 0, 5, 0,
+      6, 0, 7, 0, 8, 
+      0, 9, 0, 10, 0, 
+      11, 0, 12, 0, 13};
+  uint32_t expectedObjectsNum = 13;
+
+  std::cout << "obj nums: " << BinaryLabellingSequential::deserializeInt32(outNum.data()) << " " << expectedObjectsNum
+            << std::endl;
+
+  BinaryLabellingSequential::visualize(outV, m, n);
+
+  EXPECT_EQ(outV, expected);
+  EXPECT_EQ(BinaryLabellingSequential::deserializeInt32(outNum.data()), expectedObjectsNum);
+}
 
 TEST(SequentialBinaryLabelling, dummy3) { EXPECT_TRUE(true); }
 
