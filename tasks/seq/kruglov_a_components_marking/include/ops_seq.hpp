2f40a9cdc2fd9306ab0c9d7ded3cb74227bf1932
Alex <x.kryack.x@gmail.com>
2024-03-31 18:00:25 +0300

    first commit

---
+++ tasks/seq/kruglov_a_components_marking/include/ops_seq.hpp
@@ -0,0 +1,26 @@
+// Copyright 2023 Kruglov Alexey
+#pragma once
+
+#include <list>
+#include <random>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class imgMarkingSeq : public ppc::core::Task {
+ public:
+  explicit imgMarkingSeq(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<uint8_t>> src = {};
+  std::vector<std::vector<uint8_t>> dst = {};
+  uint32_t h{}, w{};
+  void imgMarking();
+};
