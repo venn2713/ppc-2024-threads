acbaa26da30650fac6c310075f2f65a7c708974c
Alex <x.kryack.x@gmail.com>
2024-03-31 20:33:34 +0300

    clang-tidy

--- tasks/seq/kruglov_a_components_marking/perf_tests/main.cpp
+++ tasks/seq/kruglov_a_components_marking/perf_tests/main.cpp
@@ -9,7 +9,8 @@
 TEST(kruglov_a_components_marking_seq_perf_test, test_pipeline_run) {
   // Create data
 
-  uint32_t h = 2000, w = 2000;
+  uint32_t h = 2000;
+  uint32_t w = 2000;
   std::vector<uint32_t> size = {h, w};
   std::vector<uint8_t> in(h * w, 0);
   std::vector<uint8_t> out(h * w, 0);
@@ -50,7 +51,8 @@ TEST(kruglov_a_components_marking_seq_perf_test, test_pipeline_run) {
 TEST(kruglov_a_components_marking_seq_perf_test, test_task_run) {
   // Create data
 
-  uint32_t h = 10000, w = 2000;
+  uint32_t h = 2000;
+  uint32_t w = 2000;
   std::vector<uint32_t> size = {h, w};
   std::vector<uint8_t> in(h * w, 0);
   std::vector<uint8_t> out(h * w, 0);
