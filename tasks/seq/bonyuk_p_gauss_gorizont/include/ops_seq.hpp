5c84613b9c17c26ecace8577e08e1018f656bd0b
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-04-03 21:16:50 +0300

    fdserfbdxdhf

--- tasks/seq/bonyuk_p_gauss_gorizont/include/ops_seq.hpp
+++ tasks/seq/bonyuk_p_gauss_gorizont/include/ops_seq.hpp
@@ -12,8 +12,8 @@ std::vector<int> getImage(int n, int m, uint8_t min, uint8_t max);
 class LinearGaussianFiltering : public ppc::core::Task {
  public:
   explicit LinearGaussianFiltering(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-  bool pre_processingc() override;
-  bool examination() override;
+  bool pre_processing() override;
+  bool validation() override;
   bool run() override;
   bool post_processing() override;
   int getPixel(int x, int y) { return input[x * width + y]; }
