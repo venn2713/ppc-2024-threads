455cc3d5b072f4cb3b99009d92c7dd9d9edb2055
Julia <kurdina.00@gmail.com>
2024-04-03 14:03:43 +0300

    fix mistake

--- tasks/seq/kurdina_j_linear_filter/src/ops_seq.cpp
+++ tasks/seq/kurdina_j_linear_filter/src/ops_seq.cpp
@@ -58,8 +58,8 @@ bool HorizontalSplitSequential::validation() {
 bool HorizontalSplitSequential::run() {
   internal_order_test();
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
       image[i * m + j] = NewColor(i, j);
     }
   }
