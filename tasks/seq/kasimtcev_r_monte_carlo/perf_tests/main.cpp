498eedda8cfc59c49f0d48df5a8f9ac080a2339b
Касимцев Роман <Romasias1@outlook.com>
2024-04-04 22:20:30 +0300

    Fixed New bags

--- tasks/seq/kasimtcev_r_monte_carlo/perf_tests/main.cpp
+++ tasks/seq/kasimtcev_r_monte_carlo/perf_tests/main.cpp
@@ -7,7 +7,7 @@
 #include "seq/kasimtcev_r_monte_carlo/include/ops_seq.hpp"
 #define ESTIMATE 0.2
 
-TEST(kasimtcev_r_monte_carlo, test_pipe) {
+TEST(kasimtcev_r_monte_carlo_Sequential, test_pipe) {
   double res = 8;
   func f = flinear;
 
@@ -45,121 +45,7 @@ TEST(kasimtcev_r_monte_carlo, test_pipe) {
   ASSERT_LT(std::abs(res - out[0]), ESTIMATE);
 }
 
-TEST(kasimtcev_r_monte_carlo, test_task) {
-  double res = 8;
-  func f = flinear;
-
-  std::vector<double> a1 = {0, 2};
-  std::vector<double> a2 = {0, 2};
-  std::vector<double> out(1, res);
-
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a1.data()));
-  taskDataSeq->inputs_count.emplace_back(a1.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a2.data()));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(f));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
-
-  auto testTaskSequential = std::make_shared<MonteCarloSequential>(taskDataSeq);
-
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const auto t0 = std::chrono::high_resolution_clock::now();
-  perfAttr->current_timer = [&] {
-    auto current_time_point = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-    return static_cast<double>(duration) * 1e-9;
-  };
-
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
-  perfAnalyzer->task_run(perfAttr, perfResults);
-  ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_LT(std::abs(res - out[0]), ESTIMATE);
-}
-
-TEST(kasimtcev_r_monte_carlo, test_task1) {
-  double res = 8;
-  func f = flinear;
-
-  std::vector<double> a1 = {0, 2};
-  std::vector<double> a2 = {0, 2};
-  std::vector<double> out(1, res);
-
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a1.data()));
-  taskDataSeq->inputs_count.emplace_back(a1.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a2.data()));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(f));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
-
-  auto testTaskSequential = std::make_shared<MonteCarloSequential>(taskDataSeq);
-
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const auto t0 = std::chrono::high_resolution_clock::now();
-  perfAttr->current_timer = [&] {
-    auto current_time_point = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-    return static_cast<double>(duration) * 1e-9;
-  };
-
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
-  perfAnalyzer->task_run(perfAttr, perfResults);
-  ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_LT(std::abs(res - out[0]), ESTIMATE);
-}
-
-TEST(kasimtcev_r_monte_carlo, test_task2) {
-  double res = 8;
-  func f = flinear;
-
-  std::vector<double> a1 = {0, 2};
-  std::vector<double> a2 = {0, 2};
-  std::vector<double> out(1, res);
-
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a1.data()));
-  taskDataSeq->inputs_count.emplace_back(a1.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a2.data()));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(f));
-  taskDataSeq->inputs_count.emplace_back(a2.size());
-
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
-
-  auto testTaskSequential = std::make_shared<MonteCarloSequential>(taskDataSeq);
-
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const auto t0 = std::chrono::high_resolution_clock::now();
-  perfAttr->current_timer = [&] {
-    auto current_time_point = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-    return static_cast<double>(duration) * 1e-9;
-  };
-
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
-  perfAnalyzer->task_run(perfAttr, perfResults);
-  ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_LT(std::abs(res - out[0]), ESTIMATE);
-}
-
-TEST(kasimtcev_r_monte_carlo, test_pipe1) {
+TEST(kasimtcev_r_monte_carlo_Sequential, test_task) {
   double res = 8;
   func f = flinear;
 
