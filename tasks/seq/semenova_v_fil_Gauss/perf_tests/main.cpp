5fbf2888b9b587ffd04dbd8a4646e00bdb7e86ae
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 20:17:53 +0300

    Update main.cpp

--- tasks/seq/semenova_v_fil_Gauss/perf_tests/main.cpp
+++ tasks/seq/semenova_v_fil_Gauss/perf_tests/main.cpp
@@ -65,6 +65,8 @@ TEST(semenova_v_fil_Gauss_seq, test_pipeline_run) {
       }
     }
   }
+  delete[] image;
+  delete[] filteredImage;
 }
 
 TEST(semenova_v_fil_Gauss_seq, test_task_run) {
@@ -125,4 +127,6 @@ TEST(semenova_v_fil_Gauss_seq, test_task_run) {
       }
     }
   }
+  delete[] image;
+  delete[] filteredImage;
 }
