68e1b91677e8cea1e1e8d8d0c28b827508071f64
ekistrimova <ekistrimova@gmail.com>
2024-04-05 22:10:18 +0300

    version 1.0

---
+++ tasks/seq/kistrimova_e_graham_alg/include/kistrimova_e_graham_alg_seq.hpp
@@ -0,0 +1,24 @@
+// Copyright 2024 Kistrimova Ekaterina
+#pragma once
+
+#include <vector>
+
+#include "core/task/include/task.hpp"
+namespace KistrimovaETaskSeq {
+
+using Point = std::pair<double, double>;
+
+class GrahamAlgTask : public ppc::core::Task {
+ public:
+
+  explicit GrahamAlgTask(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<Point> points;
+  std::vector<Point> finalHull;
+};
+}  // namespace KistrimovaETaskSeq
