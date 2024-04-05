4a83a24132e4d0387b72618077c9892ac895bd0c
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 18:04:54 +0300

    fix style scr

--- tasks/seq/semenova_v_fil_Gauss/src/ops_seq.cpp
+++ tasks/seq/semenova_v_fil_Gauss/src/ops_seq.cpp
@@ -4,8 +4,10 @@
 bool ImageFilGauss::validation() {
   internal_order_test();
 
-  return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() && !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr && taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 && +taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core
-}
+    return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
+         !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
+         taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
+         taskData->outputs_count[1] >= 3;  // the image size <>= size of filter core}
 
 bool ImageFilGauss::pre_processing() {
   internal_order_test();
@@ -37,7 +39,11 @@ bool ImageFilGauss::run() {
     for (int i = 1; i < n - 1; ++i) {
       for (int j = 1; j < m - 1; ++j) {
         double sum = 0;
-        sum = *imageIndex(i - 1, j - 1) * kernel[0][0] + *imageIndex(i - 1, j) * kernel[0][1] + *imageIndex(i - 1, j + 1) * kernel[0][2] + *imageIndex(i, j - 1) * kernel[1][0] + *imageIndex(i, j) * kernel[1][1] + *imageIndex(i, j + 1) * kernel[1][2] + *imageIndex(i + 1, j - 1) * kernel[2][0] + *imageIndex(i + 1, j) * kernel[2][1] + *imageIndex(i + 1, j + 1) * kernel[2][2];
+        sum = *imageIndex(i - 1, j - 1) * kernel[0][0] + *imageIndex(i - 1, j) * kernel[0][1] +
+              *imageIndex(i - 1, j + 1) * kernel[0][2] + *imageIndex(i, j - 1) * kernel[1][0] +
+              *imageIndex(i, j) * kernel[1][1] + *imageIndex(i, j + 1) * kernel[1][2] +
+              *imageIndex(i + 1, j - 1) * kernel[2][0] + *imageIndex(i + 1, j) * kernel[2][1] +
+              *imageIndex(i + 1, j + 1) * kernel[2][2];
         *filteredIndex(i, j) = (int)sum;
       }
     }
