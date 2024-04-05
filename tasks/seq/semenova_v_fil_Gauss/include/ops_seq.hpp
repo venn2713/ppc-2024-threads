c903035c813b369f01e2598b4c829588d00cd8ec
Semenova Veronika <74305462+nik-se0@users.noreply.github.com>
2024-04-05 18:08:38 +0300

    fix style hpp 2

--- tasks/seq/semenova_v_fil_Gauss/include/ops_seq.hpp
+++ tasks/seq/semenova_v_fil_Gauss/include/ops_seq.hpp
@@ -15,7 +15,7 @@ class ImageFilGauss : public ppc::core::Task {
   bool post_processing() override;
 
  private:
-    double kernel[3][3] = {
+  double kernel[3][3] = {
       {1.0 / 16, 2.0 / 16, 1.0 / 16}, {2.0 / 16, 4.0 / 16, 2.0 / 16}, {1.0 / 16, 2.0 / 16, 1.0 / 16}};
   int* image;
   int* filteredImage;
