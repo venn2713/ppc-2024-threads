98f1e40f723b68b11597431dd3e4074f6e2d9aa8
ks2k23 <lbdvks@mail.ru>
2024-04-06 04:04:23 +0300

    lebedeva_k_bitwise_sort_double_batcher_2

--- tasks/seq/lebedeva_k_bitwise_sort_double_batcher/include/ops_seq.hpp
+++ tasks/seq/lebedeva_k_bitwise_sort_double_batcher/include/ops_seq.hpp
@@ -9,8 +9,7 @@
 
 class bitwise_sort_double_batcher : public ppc::core::Task {
  public:
-  explicit bitwise_sort_double_batcher(std::shared_ptr<ppc::core::TaskData> taskData_)
-      : Task(std::move(taskData_)) {}
+  explicit bitwise_sort_double_batcher(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
